eccb6d4684b5dff3fb1da6c61a47644c507cedbd
weihc <Cfrm@123>
2021-03-02 16:56:05 +0800

    mycloud提交

---
+++ mycloud-order-service5001/src/main/java/alibaba/config/MyBatisConfig.java
@@ -0,0 +1,13 @@
+package alibaba.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @auther zzyy
+ * @create 2019-12-11 16:57
+ */
+@Configuration
+@MapperScan({"alibaba.dao"})
+public class MyBatisConfig {
+}
---
+++ mycloud-order-service5001/src/main/java/alibaba/service/OrderService.java
@@ -0,0 +1,13 @@
+package alibaba.service;
+
+
+import alibaba.domain.Order;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-26 15:19
+ */
+public interface OrderService
+{
+    void create(Order order);
+}
